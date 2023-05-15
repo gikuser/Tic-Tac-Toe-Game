6e1ec0b399817f7a00fd57ec950e8893232ee24b
gikuser <126276396+gikuser@users.noreply.github.com>
2023-05-15 22:59:31 +0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -233,3 +233,139 @@ class Play : public Board
                     }
                 }
             }
+if(this->gameOver()==false && moveIndex == side*side)
+            {
+                showBoard();
+                cout<<"It's a draw match \n\n";
+            }
+            else
+            {
+                cout<<moveIndex<<endl;
+                if(turn==PLAYER1)turn=PLAYER2;
+                else if(turn == PLAYER2)turn = PLAYER1;
+                
+                winner(turn);
+            }
+
+            cout<<"\n\n\t\t\tPress ENTER for menu\n";
+            return ;
+        }
+
+};
+
+class Game : public Play
+{   
+    private:
+        int side;
+    public :
+        Game(int side) : Play(side)
+        {
+            this->side=side;
+        }
+        void Menu()
+        {
+            int choice;
+          
+            do 
+            {    
+                cout<<"\n\n";
+                cout<<"\t\t--||-- TIC-TAC-TOE -- ||-- "<<"\n\n\n";
+                cout<<"\t\t  - - - - -  MENU - - - - -\n\n\n";
+                cout<<"\t\t  1.  INSTRUCTION\n\n";
+                cout<<"\t\t  2.  PLAY\n\n";
+                cout<<"\t\t  3.  BACK\n\n";
+                cout<<"\n\n\t\t Enter your choice : ";
+                cin>>choice;
+                switch(choice)
+                {
+                    case 1:
+                        {
+                            this->instruction();
+                            getch();
+                            getch();
+                        }
+                        break;
+                    case 2: 
+                        gameStart(PLAYER1);
+                        getch();
+                        getch();
+                        return;
+                        break;
+                    case 3:
+                        return;
+                        break;
+                    default:
+                        cout<<" \t---\tInvalid Choice\t---\t\n";
+                        cout<<"\t---  Press ENTER for menu  ---\n";
+                        getch();
+                        getch();
+                        break;
+                }
+            }while(choice!=3);
+           
+        }
+
+        void instruction()
+        {
+            cout<<"\n\n";
+            cout<<"\t\t\t\t Tic-Tac_Toe \n\n";
+            cout<<"\t\tChoose a cell no. from 1-"<<side*side<<" as below and play\n\n";
+            int p=1;
+
+            for(int i =0;i<=side*2;i++)
+            {  
+                cout<<"\t\t\t\t ";
+                for(int j=0;j<side;j++)
+                {
+                    if(i%2==0)
+                    {
+                        for(int k =0;k<side;k++)cout<<"----";
+                        break;
+                    }
+                    else
+                    {   
+                        if(j==0)cout<<"| ";
+                        cout<<p<<" | ";
+                        p++;   
+                    }
+                }
+                cout<<"\n";
+            }
+
+            cout<<"\n\n\t\t\tPress ENTER to continue. . . \n";
+
+            return;
+            
+        }
+
+};
+
+int main()
+{
+    int gridSize;
+    while(1)
+    {
+        cout<<"\n\n";
+        cout<<"\t\t\t\t\t\t\t Press 1 for EXIT"<<"\n\n\n";
+        cout<<"\t\t-- -- -- TIC-TAC-TOE -- -- -- "<<"\n\n\n";
+        cout<<"   (Note : Grid Size should be Odd no. and greater than 2)\n\n";
+        cout<<"\n\nEnter the Grid Size : ";
+        cin>>gridSize;
+
+        if(gridSize==1 || gridSize%2==0)
+        {
+            cout<<"\n\n";
+            cout<<"\t\t-- -- -- TIC-TAC-TOE -- -- -- "<<"\n\n\n";
+            cout<<"\t\tThank you for your precious time.\n";
+            cout<<"--\t--\t--\t     BYE\t    --\t--\t--\t\n";
+            break;
+        }
+
+        Game* g1 = new Game(gridSize);
+        g1->Menu();
+        delete g1;
+    }
+
+
+    return 0;
+}
